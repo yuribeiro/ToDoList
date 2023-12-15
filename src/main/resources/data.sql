ae2c1158a7a9c8eb4090983ecf4737f120a6f573
Yuri Ribeiro <120078408+yuribeiro@users.noreply.github.com>
2023-12-15 04:54:57 -0300

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,7 @@ INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS)
 VALUES ('Schedule a doctor appointment', 'Book an annual check-up with the doctor', '2023-01-18', NULL, 'in-progress');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
-VALUES ('Clean the room', 'Organize and thoroughly clean the room', '2023-01-19', NULL, 'completed');
+VALUES ('Clean the room', 'Organize and thoroughly clean the room', '2023-01-19', '2023-01-19', 'completed');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
 VALUES ('Exercise practice', 'Go to the gym or do home exercises for at least 30 minutes', '2023-01-20', NULL, 'in-progress');
@@ -20,7 +20,7 @@ INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS)
 VALUES ('Read a chapter of the current book', 'Dedicate 30 minutes to reading the book before sleeping', '2023-01-2 21:00:00', NULL, 'pending');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
-VALUES ('Study a new language', 'Practice vocabulary and grammar for 1 hour', '2023-01-22', NULL, 'completed');
+VALUES ('Study a new language', 'Practice vocabulary and grammar for 1 hour', '2023-01-22', '2023-01-22', 'completed');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
 VALUES ('Watch the latest episode of the series', 'Take time to relax and watch the favorite series', '2023-01-23', NULL, 'pending');
@@ -29,7 +29,7 @@ INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS)
 VALUES ('Prepare lunch', 'Make a healthy meal for lunch', '2023-01-24', NULL, 'in-progress');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
-VALUES ('Learn a new technology', 'Research and practice with a new programming language for 1 hour', '2023-01-25', NULL, 'completed');
+VALUES ('Learn a new technology', 'Research and practice with a new programming language for 1 hour', '2023-01-25', '2023-01-25', 'completed');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
 VALUES ('Walk the dog', 'Take the dog for a 20-minute walk', '2023-01-26', NULL, 'pending');
@@ -41,4 +41,4 @@ INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS)
 VALUES ('Respond to pending emails', 'Reply to important emails in the inbox', '2023-01-28', NULL, 'in-progress');
 
 INSERT INTO TASKSTODO (TITLE, DESCRIPTION, CREATED_AT, completed_AT, STATUS) 
-VALUES ('Learn new recipes', 'Search and save new recipes to try in the kitchen', '2023-01-29', NULL, 'completed');
+VALUES ('Learn new recipes', 'Search and save new recipes to try in the kitchen', '2023-01-29', '2023-01-29', 'completed');
